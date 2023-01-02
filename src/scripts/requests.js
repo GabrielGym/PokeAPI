a4bee41dc8fef6bb278ca02a67dbc65756da7070
Gabriel_ggm <biel.ferreira12342@gmail.com>
2023-01-02 08:14:32 -0300

    PokeAPI

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,26 +1,15 @@
-
-
-
 async function consomePokeAPI() {
-    // Seleciona o elemento que representa o loading da requisição
     const loading = document.querySelector('.conteiner-pokemons')
 
-    // Faz a requisição na API
     const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon')
       .then(
-          // Converte o retorno para um objeto Javascript válido 
         response => response.json()
       )
       .catch(
-        // Caso haja algum erro, retornamos ele no console 
         error => console.log(error)
       )
 
-    // Independente da requisição ser um sucesso, ou um erro, removeremos o loading da tela
-    loading.classList.add('hidden')
+     loading.classList.add('hidden')
 
-    // Retorna esse valor convertido
     return pokemonsDaAPI
 }
-
-// Chama a função para rodá-la ao carregar a página
